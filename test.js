d954bcc5f9dcb09535bfedaf9ac6260ebe587fdf
gardhr <gardhr@github.com>
2018-06-17 14:35:27 -0500

    restructure demo

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ try {
   var algae = { axiom: "A", rules: [["A", "AB"], ["B", "A"]] };
   var generator = lindenmayer_system(algae);
   var delay = 50;
-  setInterval(function(){process.stdout.write(generator.next())}, delay);
+  setInterval(function() {
+    process.stdout.write(generator.next());
+  }, delay);
 } catch (error) {
   console.log("Error:", error);
 }
